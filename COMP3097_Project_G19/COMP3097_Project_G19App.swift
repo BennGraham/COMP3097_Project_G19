9d645a7582acba77c3484588fe451d5d78ac2585
BennGraham <benn.graham@georgebrown.ca>
2026-03-08 21:59:34 -0400

    Initial Commit

---
+++ COMP3097_Project_G19/COMP3097_Project_G19App.swift
@@ -0,0 +1,32 @@
+//
+//  COMP3097_Project_G19App.swift
+//  COMP3097_Project_G19
+//
+//  Created by Benn Graham on 2026-03-08.
+//
+
+import SwiftUI
+import SwiftData
+
+@main
+struct COMP3097_Project_G19App: App {
+    var sharedModelContainer: ModelContainer = {
+        let schema = Schema([
+            Item.self,
+        ])
+        let modelConfiguration = ModelConfiguration(schema: schema, isStoredInMemoryOnly: false)
+
+        do {
+            return try ModelContainer(for: schema, configurations: [modelConfiguration])
+        } catch {
+            fatalError("Could not create ModelContainer: \(error)")
+        }
+    }()
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+        .modelContainer(sharedModelContainer)
+    }
+}
---
+++ COMP3097_Project_G19/Item.swift
@@ -0,0 +1,18 @@
+//
+//  Item.swift
+//  COMP3097_Project_G19
+//
+//  Created by Benn Graham on 2026-03-08.
+//
+
+import Foundation
+import SwiftData
+
+@Model
+final class Item {
+    var timestamp: Date
+    
+    init(timestamp: Date) {
+        self.timestamp = timestamp
+    }
+}
